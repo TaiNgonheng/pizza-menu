19349f3f0f624172063b86ccaabcfd0906e8b2f7
TaiNgonheng <heng68807@gmail.com>
2024-10-10 11:05:05 +0700

    component create

--- src/index.js
+++ src/index.js
@@ -1,15 +1,109 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
 
-export default function App() {
-  const x = "heng";
+function App() {
   return (
     <div>
-      <h1>Hello world from build root in react {x}</h1>
-      <p>js</p>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
+
+function Header() {
+  return (
+    <>
+      <h1>Chef Heng's Pazzy</h1>
+    </>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+    const hour = new Date().getHours();
+    const openhour = 9;
+    const closehour = 22;
+    const storehour = hour>=openhour && hour<= closehour;
+    console.log(storehour);
+    // if(storehour)
+    // alert("we're currently open");
+    // else alert("Really apologie man. That's sleepy time.");
+  return (
+    <div>
+      <footer>
+        {hour} We're Currently Opened!!!.
+      </footer>
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h4>NomKrouk</h4>
+      <p>Tomato, mozarella, spinach, and rincotta cheese</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
