d58737c1a2e2cf1162cac749a8ea56bce94f51cb
TaiNgonheng <heng68807@gmail.com>
2024-10-10 09:59:41 +0700

    render

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 export default function App() {
-  return <div>Hello world from react</div>;
+  const x = "heng";
+  return (
+    <div>
+      <h1>Hello world from build root in react {x}</h1>
+      <p>js</p>
+    </div>
+  );
 }
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
